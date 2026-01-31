bd634abc5e12782aeaf8ee337acdfebd7f219bd0
mago-mago-max <t125073@ed.sus.ac.jp>
2026-01-31 23:17:00 +0900

    保育所・小中学校表示に関する編集

--- app.py
+++ app.py
@@ -6,34 +6,75 @@ df_point = pd.read_csv('prefecture_point.csv')
 df_point['lat'] = pd.to_numeric(df_point['lat'], errors="coerce")
 df_point['lon'] = pd.to_numeric(df_point['lon'], errors="coerce")
 
-df_population = pd.read_csv('FEH_00200521_260127162349.csv',
-                 skiprows=14,
-                 encoding="utf-8")
 
-#列の抽出
-columns_needed = ['全国～人口50万以上の市','0歳','1歳', '2歳', '3歳', '4歳', '5歳', '6歳', '7歳', '8歳', '9歳', '10歳', '11歳','12歳', '13歳', '14歳']
-df_population = df_population[columns_needed]
+#<<人口>>
+# df_population = pd.read_csv('FEH_00200521_260127162349.csv',
+#                  skiprows=14,
+#                  encoding="utf-8")
 
-#数値化
-for age in range(15):
-  df_population[str(age) + '歳'] = df_population[str(age) + '歳'].str.replace(",", "").astype(int)
+# #列の抽出
+# pop_columns_needed = ['全国～人口50万以上の市','0歳','1歳', '2歳', '3歳', '4歳', '5歳', '6歳', '7歳', '8歳', '9歳', '10歳', '11歳','12歳', '13歳', '14歳']
+# df_population = df_population[pop_columns_needed]
 
-#人口合計の計算と新しい列の作成
-df_population['age_0_14'] = df_population[[str(age) + '歳' for age in range(15)]].sum(axis=1)
+# #数値化
+# for age in range(15):
+#   df_population[str(age) + '歳'] = df_population[str(age) + '歳'].str.replace(",", "").astype(int)
 
-#dataframeの変更と列の改名
-df_population.rename(columns={'全国～人口50万以上の市':'都道府県'}, inplace=True)
-df_population.drop(index=0,inplace=True)
-df_population['age_0_14'] = pd.to_numeric(df_population['age_0_14'], errors="coerce")
-df_popu_child = pd.merge(df_point, df_population[['都道府県','age_0_14']], on="都道府県", how="left")
+# #人口合計の計算と新しい列の作成
+# df_population['age_0_14'] = df_population[[str(age) + '歳' for age in range(15)]].sum(axis=1)
 
+# #dataframeの変更と列の改名
+# df_population.rename(columns={'全国～人口50万以上の市':'都道府県'}, inplace=True)
+# df_population.drop(index=0,inplace=True)
+# df_population['age_0_14'] = pd.to_numeric(df_population['age_0_14'], errors="coerce")
+# df_pop_child = pd.merge(df_point, df_population[['都道府県','age_0_14']], on="都道府県", how="left")
+
+# #pydeck Layerの作成
+# layer = pdk.Layer(
+#   "ColumnLayer",
+#   df_pop_child,
+#   get_position=['lon', 'lat'],
+#   get_elevation='age_0_14',
+#   elevation_scale=0.5,
+#   radius=20000,
+#   get_fill_color=[255, 100, 100],
+#   pickable=True,
+#   extruded=True
+# )
+
+# view_state = pdk.ViewState(
+#   latitude=36.0,
+#   longitude=138.0,
+#   zoom=4.8,
+#   bearing=0,
+#   pitch=45
+# )
+
+# map_population = pdk.Deck(
+#   layers=[layer],
+#   initial_view_state=view_state
+#   )
+
+# st.dataframe(df_population, width=800, height=220)
+# st.dataframe(df_pop_child)
+# st.pydeck_chart(map_population)
+
+
+# <<保育所・小学校・中学校>>
+df_count_nur = pd.read_csv('count_nur.csv')
+df_count_elm = pd.read_csv('count_elmsch.csv')
+df_count_jni = pd.read_csv('count_jnisch.csv')
+
+df_count = pd.concat([df_count_nur, df_count_elm, df_count_jni], ignore_index=True)
+df_count = df_count.groupby("都道府県", as_index=False)["count"].sum()
+df_fac_count = pd.merge(df_point, df_count, on="都道府県", how="left")
 
 #pydeck Layerの作成
 layer = pdk.Layer(
   "ColumnLayer",
-  df_popu_child,
+  df_fac_count,
   get_position=['lon', 'lat'],
-  get_elevation='age_0_14',
+  get_elevation='count',
   elevation_scale=0.5,
   radius=20000,
   get_fill_color=[255, 100, 100],
@@ -49,11 +90,11 @@ view_state = pdk.ViewState(
   pitch=45
 )
 
-map_population = pdk.Deck(
+map_count = pdk.Deck(
   layers=[layer],
   initial_view_state=view_state
   )
 
-st.dataframe(df_population, width=800, height=220)
-st.dataframe(df_popu_child)
-st.pydeck_chart(map_population)
+st.dataframe(df_count)
+st.pydeck_chart(map_count)
+
